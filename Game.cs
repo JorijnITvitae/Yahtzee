1209e3955c7be747742565b717bf926effbf182f
Jorijn de Graaf <jorijnitvitae@gmail.com>
2019-08-09 21:13:21 +0200

    adding dice and pattern buttons
    next is scores and game logic

---
+++ Game.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Yahtzee
+{
+    class Game
+    {
+        List<CroppedBitmap> bitmaps;
+
+        private void InitBitmaps()
+        {
+            var bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri("C:\\GitHub\\ITvitae\\Yahtzee\\resources\\dice.png");
+            bitmap.EndInit();
+
+            var empty = new CroppedBitmap(bitmap, new Int32Rect(300, 0, 100, 100));
+            var white1 = new CroppedBitmap(bitmap, new Int32Rect(0, 0, 100, 100));
+            var white2 = new CroppedBitmap(bitmap, new Int32Rect(100, 0, 100, 100));
+            var white3 = new CroppedBitmap(bitmap, new Int32Rect(200, 0, 100, 100));
+            var white4 = new CroppedBitmap(bitmap, new Int32Rect(0, 100, 100, 100));
+            var white5 = new CroppedBitmap(bitmap, new Int32Rect(100, 100, 100, 100));
+            var white6 = new CroppedBitmap(bitmap, new Int32Rect(200, 100, 100, 100));
+            var grey1 = new CroppedBitmap(bitmap, new Int32Rect(0, 200, 100, 100));
+            var grey2 = new CroppedBitmap(bitmap, new Int32Rect(100, 200, 100, 100));
+            var grey3 = new CroppedBitmap(bitmap, new Int32Rect(200, 200, 100, 100));
+            var grey4 = new CroppedBitmap(bitmap, new Int32Rect(0, 300, 100, 100));
+            var grey5 = new CroppedBitmap(bitmap, new Int32Rect(100, 300, 100, 100));
+            var grey6 = new CroppedBitmap(bitmap, new Int32Rect(200, 300, 100, 100));
+
+            bitmaps = new List<CroppedBitmap>(13);
+            bitmaps.Add(empty);
+            bitmaps.Add(white1); bitmaps.Add(white2); bitmaps.Add(white3);
+            bitmaps.Add(white4); bitmaps.Add(white5); bitmaps.Add(white6);
+            bitmaps.Add(grey1); bitmaps.Add(grey2); bitmaps.Add(grey3);
+            bitmaps.Add(grey4); bitmaps.Add(grey5); bitmaps.Add(grey6);
+        }
+
+        private class Die
+        {
+            public Game game;
+            public CroppedBitmap bitmap;
+            public bool locked;
+            public int number;
+
+            public void Reset()
+            {
+                bitmap = game.bitmaps[0];
+                locked = false;
+                number = 0;
+            }
+
+            public Die(Game thisGame)
+            {
+                game = thisGame;
+                Reset();
+            }
+
+            private void UpdateBitmap()
+            {
+                if (number > 0)
+                {
+                    if (locked)
+                        bitmap = game.bitmaps[number + 6];
+                    else
+                        bitmap = game.bitmaps[number];
+                }
+                else
+                {
+                    bitmap = game.bitmaps[0];
+                }
+            }
+
+            public void Toggle()
+            {
+                if (number > 0)
+                    locked = !locked;
+
+                UpdateBitmap();
+            }
+
+            public void Roll()
+            {
+                if (!locked)
+                {
+                    var random = new Random(Guid.NewGuid().GetHashCode());
+                    number = random.Next(1, 7);
+                    UpdateBitmap();
+                }
+            }
+        }
+
+        private List<Die> dice;
+
+        private void InitDice()
+        {
+            dice = new List<Die>(5);
+            for (int i = 0; i < 5; i++)
+                dice.Add(new Die(this));
+        }
+
+        public Game()
+        {
+            InitBitmaps();
+            InitDice();
+        }
+
+        public int NumberOfDice => dice.Count;
+
+        public CroppedBitmap DefaultBitmap => bitmaps[0];
+
+        public CroppedBitmap Toggle(int d)
+        {
+            dice[d - 1].Toggle();
+            return dice[d - 1].bitmap;
+        }
+
+        public CroppedBitmap Roll(int d)
+        {
+            dice[d - 1].Roll();
+            return dice[d - 1].bitmap;
+        }
+
+        public CroppedBitmap GetBitmap(int d)
+        {
+            return dice[d - 1].bitmap;
+        }
+
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,62 +20,51 @@ namespace Yahtzee
     /// </summary>
     public partial class MainWindow : Window
     {
-        BitmapImage bitmap;
-        List<CroppedBitmap> bitmaps;
+        Game game;
 
         public MainWindow()
         {
             InitializeComponent();
+            game = new Game();
 
-            bitmap = new BitmapImage();
-            bitmap.BeginInit();
-            bitmap.UriSource = new Uri("C:\\GitHub\\ITvitae\\Yahtzee\\resources\\dice.png");
-            bitmap.EndInit();
-
-            bitmaps = new List<CroppedBitmap>(13);
-            CroppedBitmap empty  = new CroppedBitmap(bitmap, new Int32Rect(300,   0, 100, 100));
-            CroppedBitmap white1 = new CroppedBitmap(bitmap, new Int32Rect(  0,   0, 100, 100));
-            CroppedBitmap white2 = new CroppedBitmap(bitmap, new Int32Rect(100,   0, 100, 100));
-            CroppedBitmap white3 = new CroppedBitmap(bitmap, new Int32Rect(200,   0, 100, 100));
-            CroppedBitmap white4 = new CroppedBitmap(bitmap, new Int32Rect(  0, 100, 100, 100));
-            CroppedBitmap white5 = new CroppedBitmap(bitmap, new Int32Rect(100, 100, 100, 100));
-            CroppedBitmap white6 = new CroppedBitmap(bitmap, new Int32Rect(200, 100, 100, 100));
-            CroppedBitmap grey1  = new CroppedBitmap(bitmap, new Int32Rect(  0, 200, 100, 100));
-            CroppedBitmap grey2  = new CroppedBitmap(bitmap, new Int32Rect(100, 200, 100, 100));
-            CroppedBitmap grey3  = new CroppedBitmap(bitmap, new Int32Rect(200, 200, 100, 100));
-            CroppedBitmap grey4  = new CroppedBitmap(bitmap, new Int32Rect(  0, 300, 100, 100));
-            CroppedBitmap grey5  = new CroppedBitmap(bitmap, new Int32Rect(100, 300, 100, 100));
-            CroppedBitmap grey6  = new CroppedBitmap(bitmap, new Int32Rect(200, 300, 100, 100));
-            bitmaps.Add( empty);
-            bitmaps.Add(white1); bitmaps.Add(white2); bitmaps.Add(white3);
-            bitmaps.Add(white4); bitmaps.Add(white5); bitmaps.Add(white6);
-            bitmaps.Add( grey1); bitmaps.Add( grey2); bitmaps.Add( grey3);
-            bitmaps.Add( grey4); bitmaps.Add( grey5); bitmaps.Add( grey6);
+            Image_Dice1.Source = game.DefaultBitmap;
+            Image_Dice2.Source = game.DefaultBitmap;
+            Image_Dice3.Source = game.DefaultBitmap;
+            Image_Dice4.Source = game.DefaultBitmap;
+            Image_Dice5.Source = game.DefaultBitmap;
         }
 
-        private void Button_Dice1_Click(object sender, RoutedEventArgs e)
+        private void Button_Dice_Click(object sender, RoutedEventArgs e)
         {
-            Image_Dice1.Source = bitmaps[1];
+            Button button = (Button)(sender);
+            switch (button.Content)
+            {
+                case "1":
+                    Image_Dice1.Source = game.Toggle(1); break;
+                case "2":
+                    Image_Dice2.Source = game.Toggle(2); break;
+                case "3":
+                    Image_Dice3.Source = game.Toggle(3); break;
+                case "4":
+                    Image_Dice4.Source = game.Toggle(4); break;
+                case "5":
+                    Image_Dice5.Source = game.Toggle(5); break;
+            }
         }
 
-        private void Button_Dice2_Click(object sender, RoutedEventArgs e)
+        private void Button_Roll_Click(object sender, RoutedEventArgs e)
         {
-            Image_Dice2.Source = bitmaps[2];
+            Image_Dice1.Source = game.Roll(1);
+            Image_Dice2.Source = game.Roll(2);
+            Image_Dice3.Source = game.Roll(3);
+            Image_Dice4.Source = game.Roll(4);
+            Image_Dice5.Source = game.Roll(5);
         }
 
-        private void Button_Dice3_Click(object sender, RoutedEventArgs e)
+        private void Button_Pattern_Click(object sender, RoutedEventArgs e)
         {
-            Image_Dice3.Source = bitmaps[3];
-        }
-
-        private void Button_Dice4_Click(object sender, RoutedEventArgs e)
-        {
-            Image_Dice4.Source = bitmaps[4];
-        }
-
-        private void Button_Dice5_Click(object sender, RoutedEventArgs e)
-        {
-            Image_Dice5.Source = bitmaps[5];
+            Button button = (Button)(sender);
+            button.IsEnabled = false;
         }
     }
 }
